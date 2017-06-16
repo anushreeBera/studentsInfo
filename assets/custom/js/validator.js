23ffa2bbe37365a89e2b04e94aaa4194da280d63
anushreeBera <anushreejsr@gmail.com>
2017-06-16 17:48:41 +0530

    Custom styling added and basic js

---
+++ assets/custom/js/validator.js
@@ -0,0 +1,9 @@
+/*
+Creation date: 16-6-2017
+Purpose:  adding validations to the HTML page registration_form.html created in Bootstrap
+File Path: /studentsInfo-Bootstrap/assets/custom/js/validator.js
+Created By : Anushree.
+*/
+$(document).ready(function(){
+	alert('Hello Student, register yourself here.');
+});
