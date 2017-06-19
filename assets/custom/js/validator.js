31a08cfdedd125ee7cac1c838f156e0d869a0c15
anushreeBera <anushreejsr@gmail.com>
2017-06-19 09:34:39 +0530

    Design changes as in, in-line input for sm

--- assets/custom/js/validator.js
+++ assets/custom/js/validator.js
@@ -4,6 +4,9 @@ Purpose:  adding validations to the HTML page registration_form.html created in 
 File Path: /studentsInfo-Bootstrap/assets/custom/js/validator.js
 Created By : Anushree.
 */
-$(document).ready(function(){
-	alert('Hello Student, register yourself here.');
-});
+(function($, window, document){
+
+	$(function(){
+		//alert("Hello, student.");
+	});
+}(window.jQuery, window, document));
