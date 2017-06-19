dbc1fe755a9d954b0f25087595e2f0a37292e8ed
anushreeBera <anushreejsr@gmail.com>
2017-06-19 17:56:27 +0530

    Validations done!

--- assets/custom/js/validator.js
+++ assets/custom/js/validator.js
@@ -10,8 +10,8 @@ Created By : Anushree.
 	$(function(){
 		
 		alert("Hello, student.");
-		
-		//to enable/disable submit on check/uncheck
+
+		//to enable/disable submit on check/uncheck of i agree
 		$(function(){
 			$('#iAgree').click(function() {
 				if ($(this).is(':checked')) {
@@ -24,19 +24,33 @@ Created By : Anushree.
 			});
 		});
 		
+		//to validate the form on click of submit
+		$(function(){
+			$('#submitButton').click(function(){
+				validateForm();   
+			});
+		});
+		
+		//to clear error messages and disable submit button on click of reset
+		$(function(){
+			$('#resetButton').click(function(){
+				$("span").html("");
+				$('#submitButton').attr('disabled', 'disabled');
+			});
+		});
+		
 		
 	});
 }(window.jQuery, window, document));
 
 function validateForm() {
 
-
     var nameReg = /^[A-Za-z]+$/;
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    var emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 	var alphaNumericReg = /^[a-z0-9]+$/i;
 	
 	//fetching form elements in an array
-	var formCollection = document.getElementById("form").elements;
+	var formCollection = document.getElementById("registrationForm").elements;
 	var totalFields = formCollection.length;
 	
     var fName = $('#firstName').val();
@@ -50,47 +64,69 @@ function validateForm() {
 
     var inputVal = new Array(fName, mName, lName, dob, email, username, pass1, pass2);
 
-    var inputMessage = new Array("first name", "middle name", "last name", "date of birth", "email address", "username", "password", "password");
+    var inputMessage = new Array("first name", "middle name", "last name", "date of birth", "email address", "username", "password", "password again");
 
-     $('.error').hide();
+    $('.error').hide();
 
-        if(inputVal[0] == ""){
-            $('#messages').after('<span class="error"> Please enter your ' + inputMessage[0] + '</span>');
-        } 
-        else if(!nameReg.test(inputVal[0])){
-            $('#messages').after('<span class="error"> Letters only!</span>');
-        }
+    if(inputVal[0] == ""){
+        $('#messages').after('<span class="error"> Please enter your ' + inputMessage[0] + '</span>');	
+		return false;
+    } 
+    else if(!nameReg.test(inputVal[0])){
+        $('#messages').after('<span class="error"> Letters only!</span>');
+		return false;
+    }
 
-        if(!nameReg.test(inputVal[1])){
-            $('#messages').after('<span class="error"> Letters only!</span>');
-        }
+	if( (inputVal[1] != "") && !nameReg.test(inputVal[1]) ){
+        $('#messages').after('<span class="error"> Letters only!</span>');
+		return false;
+    }
 
-        if(inputVal[2] == ""){
-            $('#messages').after('<span class="error"> Please enter your ' + inputMessage[2] + '</span>');
-        }
-		else if(!nameReg.test(inputVal[2])){
-            $('#messages').after('<span class="error"> Letters only!</span>');
-        }
+    if(inputVal[2] == ""){
+        $('#messages').after('<span class="error"> Please enter your ' + inputMessage[2] + '</span>');
+		return false;
+    }
+	else if(!nameReg.test(inputVal[2])){
+        $('#messages').after('<span class="error"> Letters only!</span>');
+		return false;
+    }
 		
-        if(!emailReg.test(inputVal[4])){
-            $('#messages').after('<span class="error"> Please enter a valid email address</span>');
-        }
+	if(inputVal[3] == ""){
+        $('#messages').after('<span class="error"> Please enter your ' + inputMessage[3] + '</span>');
+		return false;
+    }
+		
+	if(inputVal[4] == ""){
+        $('#messages').after('<span class="error"> Please enter your ' + inputMessage[4] + '</span>');
+		return false;
+    }
+    else if(!emailReg.test(inputVal[4])){
+		$('#messages').after('<span class="error"> Please enter a valid ' + inputMessage[4] + '</span>');
+		return false;
+    }
 
-        if(inputVal[5] == ""){
-            $('#messages').after('<span class="error"> Please enter your ' + inputMessage[5] + '</span>');
-        } 
-        else if(!alphaNumericReg.test(inputVal[5])){
-            $('#messages').after('<span class="error"> Numbers only</span>');
-        }
+    if(inputVal[5] == ""){
+		$('#messages').after('<span class="error"> Please enter your ' + inputMessage[5] + '</span>');
+		return false;
+    } 
+    else if(!alphaNumericReg.test(inputVal[5])){
+        $('#messages').after('<span class="error"> Letters and Numbers only!</span>');
+		return false;
+    }
 
-        if(inputVal[6] == ""){
-            $('#messages').after('<span class="error"> Please enter your ' + inputMessage[6] + '</span>');
-        }
+    if(inputVal[6] == ""){
+        $('#messages').after('<span class="error"> Please enter your ' + inputMessage[6] + '</span>');
+		return false;
+    }
 		
-		
-		
-		if(inputVal[7] == ""){
-            $('#messages').after('<span class="error"> Please enter your ' + inputMessage[7] + '</span>');
-        }
+	if(inputVal[7] == ""){
+        $('#messages').after('<span class="error"> Please enter your ' + inputMessage[7] + '</span>');
+		return false;
+    }
+	else if(inputVal[6] != inputVal[7]){
+        $('#messages').after('<span class="error"> Password must be same!</span>');
+		return false;
+    }
+
 		
 }   
